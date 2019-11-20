4eafac036f84722674289730bc52a85a5b6aac31
edumoreira1506 <contato@eduardoem.com.br>
2019-11-19 21:10:40 -0300

    Install spring

---
+++ src/main/java/br/edu/utfpr/contratedev/ApiContrateDevApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.contratedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContrateDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContrateDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/contratedev/ApiContrateDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.contratedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContrateDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
